22970068d687824c0c86db4d7356eeb82c21b924
westbanana <westbanana4@gmail.com>
2023-01-25 17:59:22 +0200

    i

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+import style from './style.module.scss';
+
+const App = () => {
+  const [userData, setUserData] = useState({
+    name: localStorage.getItem('userName') ? localStorage.getItem('userName') : '',
+    todosCount: localStorage.getItem('todosCount') ? localStorage.getItem('todosCount') : 0,
+  });
+  const [inputUserName, setInputUserName] = useState('');
+  const createUser = () => {
+    if (inputUserName) {
+      let userNameFirstLetterUpperCase = inputUserName;
+      inputUserName.split('').forEach((e, i) => {
+        if (i === 0 || inputUserName[i - 1] === ' ') {
+          userNameFirstLetterUpperCase = userNameFirstLetterUpperCase.substring(0, i) + e.toUpperCase()
+          + userNameFirstLetterUpperCase.substring(i + 1);
+        }
+      });
+      setUserData({
+        name: userNameFirstLetterUpperCase,
+        todosCount: userData.todosCount,
+      });
+      localStorage.setItem('userName', userData.name);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React
-        </a>
+    <div className={style.main}>
+      <header>
+        <h1>{`${userData.name} ToDo list`}</h1>
       </header>
+      <body>
+        {userData.name !== ' ' && (
+        <div className={style.authorize}>
+          <form action="#">
+            <input
+              type="text"
+              placeholder="Set username"
+              value={inputUserName}
+              required
+              onChange={e => setInputUserName(e.target.value)}
+            />
+            <button
+              onClick={createUser}
+              type="submit"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+        )}
+      </body>
     </div>
   );
-}
+};
 
 export default App;

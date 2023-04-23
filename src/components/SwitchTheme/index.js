34e0e284cd9cd42ff9098f143e8d2e56d69eb717
westbanana <westbanana4@gmail.com>
2023-04-23 22:34:41 +0300

    change swipe theme

--- src/components/SwitchTheme/index.js
+++ src/components/SwitchTheme/index.js
@@ -1,28 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import styles from './style.module.scss';
 
 const SwitchTheme = () => {
-  const [light, setLight] = useState(false);
-  const swipeTheme = () => {
-    if (light) {
-      return {
-        left: '0%',
-      };
-    }
-    return {
-      left: 'calc(100% - 20px)',
-    };
-  };
+  console.log('switch');
   return (
     <div
-      onClick={() => setLight(prev => !prev)}
-      role="presentation"
       className={styles.mainBlock}
     >
       <div
         className={styles.circle}
-        style={swipeTheme()}
       />
     </div>
   );

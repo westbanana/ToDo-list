da5f8e91688d742e8f8a52d8d26c47e830b82464
westbanana <westbanana4@gmail.com>
2023-04-23 21:40:10 +0300

    chage getData function

--- src/components/Helpers/GetData.js
+++ src/components/Helpers/GetData.js
@@ -1,10 +1,13 @@
-const data = new Date();
+const getData = () => {
+  const data = new Date();
+  const monthShortNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
+  ];
+  return (
+    `${data.getDate()}/${monthShortNames[data.getMonth()]}/${data.getFullYear()}/${data.getHours()}:${data.getMinutes() >= 10
+      ? data.getMinutes()
+      : `0${data.getMinutes()}`}`
+  );
+};
 
-const monthShortNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-  'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec',
-];
-const todoData = `${data.getDate()}/${monthShortNames[data.getMonth()]}/${data.getFullYear()}/${data.getHours()}:${data.getMinutes() > 10
-  ? data.getMinutes()
-  : `0${data.getMinutes()}`}`;
-
-export default todoData;
+export default getData;
